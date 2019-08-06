9d1907fe248be0b15be05f092348179dad206b65
ajinbramantio <ajinbramantio@gmail.com>
2019-08-07 01:36:03 +0700

    Add Logout Function

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
-import {Logo as BaseLogo} from "pages/Home"
+import { Logo as BaseLogo } from 'pages/Home'
+
+import firebase from 'fire'
 
 const Navbar = styled.div`
-  background: #1D3557;
+  background: #1d3557;
   color: #fefefe;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 1rem;
 `
 
 const Logo = styled(BaseLogo)`
   font-size: 2rem;
 `
 
-const Logout = styled(Link)`
+const Logout = styled.button`
+  padding: 1rem;
+  border: none;
   color: #fff;
   font-weight: 800;
-  text-decoration: none; 
+  text-decoration: none;
 `
 
 const Container = styled.div`
@@ -30,7 +33,7 @@ const Container = styled.div`
 
 const ActionButton = styled.button`
   padding: 0.75rem 1rem;
-  background: #00A8E8;
+  background: #00a8e8;
   color: #fcfcfc;
   border: none;
   border-radius: 5px;
@@ -38,23 +41,36 @@ const ActionButton = styled.button`
 `
 
 class Dashboard extends React.Component {
+  state = {
+    isLogoutProccessed: false
+  }
+
+  handleLogout = () => {
+    const self = this
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        self.setState({
+          isLogoutProccessed: true
+        })
+      })
+  }
+
   render() {
-    return (
+    return this.state.isLogoutProccessed ? (
+      <Redirect to="/" />
+    ) : (
       <React.Fragment>
         <Navbar>
           <Logo />
-           <div>
-             <span>Raga Subekti</span> | <Logout to="/logout">Logout</Logout>
-           </div>
+          <div>
+            <span>Raga Subekti</span> |{' '}
+            <Logout onClick={this.handleLogout}>Logout</Logout>
+          </div>
         </Navbar>
         <Container>
           <ActionButton onClick={() => {}}>Add Poll</ActionButton>
-
-          <div>
-
-
-
-          </div>
         </Container>
       </React.Fragment>
     )
