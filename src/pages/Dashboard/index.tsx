20ccaf544fc47fb6bab6801409fc130d7c18793e
ajinbramantio <ajinbramantio@gmail.com>
2019-08-07 01:37:33 +0700

    Add Missing Import

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import styled from '@emotion/styled'
 
-import { Link } from 'react-router-dom'
+import { Link, Redirect } from 'react-router-dom'
 
 import { Logo as BaseLogo } from 'pages/Home'
 
